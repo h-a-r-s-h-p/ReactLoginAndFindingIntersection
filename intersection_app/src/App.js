c01b2ae606558afe56e089885d0a4aeea8573195
harsh <harshparihar1706@gmail.com>
2022-09-16 16:12:42 +0530

    added profile and verify page

--- intersection_app/src/App.js
+++ intersection_app/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import './App.css';
 import {Route, Routes, BrowserRouter} from 'react-router-dom';
 import Home from './Home/Home';
+import Profile from './Profile/Profile';
+import Verify from './Verify/Verify';
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <div>
         <Routes>                                            {/* Routes is wrapped because everything outside routes will be present in every component */}
           <Route exact path="/" element={<Home />} />
-
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/verify" element={<Verify />}/>
         </Routes>
       </div>
       <footer> @author Harsh Parihar</footer>
--- intersection_app/src/Home/Home.jsx
+++ intersection_app/src/Home/Home.jsx
@@ -1,26 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./Home.css";
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: null,
-            password: null,
-            SignUpOrSignIn: "Sign In"
-        };
 
-        // this.handleChange = this.handleChange.bind(this);
-        // this.handleSubmit = this.handleSubmit.bind(this);
+import {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
+function Home(props) {
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [SignUpOrSignIn, setSignUpSignIn] =useState("Sign In");
+    
+    let navigate = useNavigate();
+    const handleSubmit= ()=>{
+        if(SignUpOrSignIn==="Sign In"){
+            navigate("/profile")
+        }
+        else{
+            navigate("/verify")
+        }
     }
 
+    function handleChange(event) {
+        var name = event.target.name;
+        var value = event.target.value;
+        this.setState({
+          [name]: value
+        });
+      }
 
-    render() {
-        let SignIn_btn_class = this.state.SignUpOrSignIn === "Sign In" ? "blueButton" : "whiteButton";
-        let SignUp_btn_class = this.state.SignUpOrSignIn === "Sign Up" ? "blueButton" : "whiteButton";
+        let SignIn_btn_class = SignUpOrSignIn === "Sign In" ? "blueButton" : "whiteButton";
+        let SignUp_btn_class = SignUpOrSignIn === "Sign Up" ? "blueButton" : "whiteButton";
         return (
-            <body>
-                {console.log(`SignUpOrSignIn = ` + this.state.SignUpOrSignIn)}
+            <div>
+                {console.log(`SignUpOrSignIn = ` + SignUpOrSignIn)}
                 {console.log(`SignIn_btn_class = ` + SignIn_btn_class)}
                 {console.log(`SignIn_Up_class = ` + SignUp_btn_class)}
                 <div id="container">
@@ -30,18 +41,24 @@ class Home extends Component {
                             <form >
                                 <div class="form-group">
                                     <div class="email-input-box">
-                                        <input placeholder="Enter Email" type="email"
-                                            class="form-control" formControlName="email" />
+                                        <input  placeholder="Enter Email" 
+                                                type="email"
+                                                class="form-control" 
+                                                formControlName="email"
+                                                onChange={handleChange} />
                                     </div>
 
                                     <div class="password-input-box">
-                                        <input placeholder="Enter Password" type="password"
-                                            class="form-control" formControlName="password" />
+                                        <input  placeholder="Enter Password"
+                                                type="password"
+                                                class="form-control" 
+                                                formControlName="password"
+                                                onChange={handleChange} />
                                     </div>
 
                                 </div>
 
-                                <button class="btn btn-success" type="submit">{this.state.SignUpOrSignIn}</button>
+                                <button class="btn btn-success" type="submit" onClick={handleSubmit}>{SignUpOrSignIn}</button>
 
                             </form>
                         </div >
@@ -51,16 +68,16 @@ class Home extends Component {
                 <div id="outer-container">
                     <button className={SignIn_btn_class}
                         onClick={() => {
-                            this.setState({ SignUpOrSignIn: "Sign In" })
+                            setSignUpSignIn("Sign In")
                         }}> "Sign In" </button>
                     <button className={SignUp_btn_class}
                         onClick={() => {
-                            this.setState({ SignUpOrSignIn: "Sign Up" })
+                            setSignUpSignIn("Sign Up")
                         }}> "Sign Up" </button>
                 </div>
-            </body >
+            </div >
         );
-    }
+
 }
 
 export default Home;
