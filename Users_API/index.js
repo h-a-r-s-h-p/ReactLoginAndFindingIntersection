6027a79aa1c40640e74acccbdaf81bb3cc146f2e
harsh <harshparihar1706@gmail.com>
2022-09-16 18:15:02 +0530

    backend created and database linked

---
+++ Users_API/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes/routes')
+const app = express();
+
+app.use('/',routes)
+require('dotenv').config();
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error',(error) =>{
+    console.log(error)
+})
+database.once('connected',()=>{
+    console.log('Database Connected')
+})
+
+app.use(express.json());
+app.listen(3001,()=>{
+    console.log(`Server Started at ${3001}`)
+})
